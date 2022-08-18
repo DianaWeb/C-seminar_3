60a1df06e3e9fdcc18ff7f92e2647b0854268a5f
«Diana» <diana-mk@ya.ru>
2022-08-18 22:23:09 +0700

    Решили три задачи

---
+++ task18/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите номер четверти от 1 до 4: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+	System.Console.WriteLine("Диапазон координат: x > 0 && y > 0");
+}
+else if (num == 2)
+{
+	System.Console.WriteLine(" Диапазон координат: x < 0 && y > 0");
+}
+else if (num == 3)
+{
+	System.Console.WriteLine(" Диапазон координат: x < 0 && y < 0");
+}
+else if (num == 4)
+{
+	System.Console.WriteLine(" Диапазон координат: x > 0 && y < 0");
+}
+else
+{
+	System.Console.WriteLine("Диапазон невозможно определить.");
+}
---
+++ task20/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 20: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (3,6); B(2, 1)-> 5,09
+A(7, -5); B(1, -1)-> 7,21 */
+System.Console.WriteLine("введите Х1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите Х2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите Y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите Y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+System.Console.WriteLine($"{distance:f2}");
