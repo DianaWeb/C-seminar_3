2727cb79f49b2590af67988c1b1c41bbfabfc33f
«Diana» <diana-mk@ya.ru>
2022-08-18 20:30:31 +0700

    Создали шаблон для 17 задачи и файл gitignore

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите координыту точки Х");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координыту точки Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+	System.Console.WriteLine(" В первой координатной четверти");
+}
+else if (x < 0 && y > 0)
+{
+	System.Console.WriteLine(" Во второй координатной четверти");
+}
+else if (x < 0 && y < 0)
+{
+	System.Console.WriteLine(" В третьей координатной четверти");
+}
+else if (x > 0 && y < 0)
+{
+	System.Console.WriteLine(" В четвертой координатной четверти");
+}
+else
+{
+	System.Console.WriteLine("Координатную четверть невозможно определить.");
+}
